c7a7e01d9689cfd9707dec673a203a36f80ea290
Hunor Nagy <78656999+Huni111@users.noreply.github.com>
2024-03-05 23:26:42 +0000

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -28,7 +28,7 @@ const [loaded, setLoaded] = useState(false)
         setError(err);
         setRecipe(null);
         setLoaded(true)
-        console.log(`ERROR FETCHING DATA! ${error.message}`);
+        throw new Error("failed fetching"+err);
 
 
        }
