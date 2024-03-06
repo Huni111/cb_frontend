6f4261e64c3a2ed9bd55999c1cd85f90bb977418
Hunor Nagy <78656999+Huni111@users.noreply.github.com>
2024-03-06 18:07:38 +0000

    Update RecipeDetails.jsx

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -8,7 +8,7 @@ const DetailPage = () => {
 
   const fetchRecipe = async () => {
     try {
-      const response = await fetch(`https://cook-book-server.onrender.com/api/recipe/recipes/${recipeId}`);
+      const response = await fetch(import.meta.env.VITE_API_ENDPOINT_FINDONE${recipeId}`);
       const data = await response.json();
       setRecipe(data);
       setError(null);
