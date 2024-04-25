94b33d847dc239d11da4fb33eac3b8b48a7165c2
Huni111 <huni19966@gmail.com>
2024-04-25 21:05:09 +0100

    lang button invented

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,19 +2,7 @@ import React, { useState } from "react";
 import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from 'react-router-dom'
 
-/**
- * The `Landing` component is the main entry point of the application, providing a landing page with various navigation options.
- * 
- * The component includes the following features:
- * - A large "Cook-Book" title
- * - Buttons for registration, login, and accessing recipes
- * - A toggle menu button that opens a side menu with the same navigation options
- * - A greeting message with a link to the recipes page
- * - A pot image
- * - A copyright notice
- * 
- * The component uses the `useState` hook to manage the state of the toggle menu, and the `Link` component from `react-router-dom` to provide navigation links.
- */
+
 export default function Landing() {
     const [toggleActive, setToggle] = useState(false)
 
@@ -24,13 +12,22 @@ export default function Landing() {
 
     return (
         <>
-            <div className='landing_container'>
-                <div className='landing_sec'>
+            <div className='landing_container '>
+            <div className="lang_select">
+            <label>language:</label>
+                    <select>
+                        <option value={'hu'}>Magyar</option>
+                        <option value={'en'}>English</option>
+                        <option value={'ro'}>Română</option>
+                    </select>
+                    </div>
+                <div className='landing_sec first_landing'>
                     <h1 className="text-9xl text-left font-bold cim">Cook-Book</h1>
                     <div className='button_container'>
                         <button>  <Link className='landing_buttons text-3xl' to="/app/registration">Regisztráció</Link></button>
                         <button>  <Link className='landing_buttons text-3xl' to="/app/login">Bejelentkezés</Link></button>
                         <button> <Link className='landing_buttons text-3xl' to="/app/">Receptek</Link></button>
+                        
                     </div>
                     <button onClick={handleToggle} className="toggle_menu"><MenuIcon />Menü</button>
 
@@ -49,6 +46,7 @@ export default function Landing() {
 
                 <div className='landing_sec'>
                     <h5 style={{ textShadow: '1px 1px 2px black' }}>Copyright Hunor Nagy 2024</h5>
+                    
                 </div>
             </div>
 
